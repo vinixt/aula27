fcd7b6102784156617e6dc897ff76ad23da26043
vinixt <121584111+vinixt@users.noreply.github.com>
2023-02-27 15:48:48 -0300

    Update Tower.js

--- aula27-main/aula27-main/ClassC27-SA-main/Tower.js
+++ aula27-main/aula27-main/ClassC27-SA-main/Tower.js
@@ -3,10 +3,11 @@ class Tower{
         var options={
             isStatic:true
         }
+        this.image=loadImage("assets/tower.png");
         this.w=w;
         this.h=h;
         this.body=Bodies.rectangle(x,y,this.w,this.h,options);
       World.add(world,this.body)  
     }
-
+mostrar(){ var pos = this.body.position; var angle=this.body.angle retto (pos.x, pos.y, this.w, this.h) }
 }
