54baf10f7e899cdac1530419731091164822e50d
vinixt <121584111+vinixt@users.noreply.github.com>
2023-02-27 15:33:43 -0300

    Add files via upload

---
+++ aula27-main/aula27-main/ClassC27-SA-main/Tower.js
@@ -0,0 +1,12 @@
+class Tower{
+    constructor(x,y,w,h ){
+        var options={
+            isStatic:true
+        }
+        this.w=w;
+        this.h=h;
+        this.body=Bodies.rectangle(x,y,this.w,this.h,options);
+      World.add(world,this.body)  
+    }
+
+}
